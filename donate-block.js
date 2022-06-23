7ab920b7f361a9823126d5b78ac2bdd2c66d0f84
Vladislav <vadimromashko@gmail.com>
2022-06-23 23:06:48 +0300

    finish project

--- donate-block.js
+++ donate-block.js
@@ -1,9 +1,17 @@
+import { getTodayDateFormat } from './utils/data'
+
 export class DonateBlock {
-    constructor() {
-        
-    }
+  #donateItem
+  constructor() {
+    this.#donateItem = document.createElement('div')
+    this.#donateItem.className = 'donate-item'
+  }
 
-    render() {
+  render(value) {
+    this.#donateItem.innerHTML = `${getTodayDateFormat(
+      new Date()
+    )} - <b>${value}$</b>`
 
-    }
+    return this.#donateItem
+  }
 }
--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-import './index.css';
-import {getTodayDateFormat} from './utils/data'
+import './index.css'
+import { DonateBlock } from './donate-block'
 
-
-
-const donatesContainer= document.querySelector('.donates-container__donates')
-const donatesForm= document.querySelector('.donate-form')
-const input= donatesForm.querySelector('.donate-form__donate-input')
+const donatesContainer = document.querySelector('.donates-container__donates')
+const donatesForm = document.querySelector('.donate-form')
+const input = donatesForm.querySelector('.donate-form__donate-input')
 const totalAmount = donatesForm.querySelector('#total-amount')
 let totalSum = 0
 
-donatesForm.addEventListener('submit', event=>{
-    event.preventDefault()
+donatesForm.addEventListener('submit', (event) => {
+  event.preventDefault()
 
-    const donateItem = document.createElement('div')
-    donateItem.className = 'donate-item'
-    donateItem.innerHTML = `${getTodayDateFormat(new Date())} - <b>${input.value}$</b>`
-    donatesContainer.append(donateItem)
-    let num = Number(input.value).toFixed(2)
-    console.log(num)
-    totalSum += parseFloat(num)
-    totalAmount.innerText = `${totalSum}$`
-    input.value = ''
-    
-    
+  const donateItem = new DonateBlock()
+  donatesContainer.append(donateItem.render(input.value))
+
+  totalSum += Number(input.value)
+  totalAmount.innerText = `${totalSum}.00 $`
+  input.value = ''
 })
-
